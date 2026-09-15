928e05eee830d4a9e08b00c94e5f934a399a4f9c
Mohammed Rif Ahsan <rif-x43@users.noreply.github.com>
2026-09-15 23:56:11 +0600

    Update splash_screen.dart

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -13,34 +13,34 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future<void>.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 3), () {
       if (!mounted) return;
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute<void>(builder: (_) => const LoginScreen()),
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
       );
     });
   }
 
-  @override
+@override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Stack(
-        children: [
-          DecoratedBox(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Colors.transparent, Color(0x332F8DF6)],
-              ),
-            ),
-            child: SizedBox.expand(),
+      backgroundColor: Color(0xFFF8F8FF),
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.transparent, Color(0x332F8DF6)],
           ),
-          Align(
-            alignment: Alignment(0, -0.07),
-            child: Image(image: AssetImage('brandings/icon.png'), height: 120),
+        ),
+        child: Align(
+          alignment: Alignment(0, -0.07),
+          child: Image(
+            image: AssetImage('brandings/icon.png'),
+            height: 120,
           ),
-        ],
+        ),
       ),
     );
   }
